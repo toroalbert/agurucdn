abbaa6564a0bb9c31d61f75fcc03e7ab4a85fbff
Alberto Toro <toroalbert@gmail.com>
2023-12-25 17:49:08 -0400

    UpdateCDN

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ app.config(function ($routeProvider) {
         })
         .when('/register', {
             templateUrl: './views/register.html',
-            controller: 'LoginController'
+            controller: 'RegisterController'
         })
         .when('/forgot-password', {
             templateUrl: './views/forgot-password.html',
@@ -52,6 +52,11 @@ app.config(function ($routeProvider) {
         .when('/buttons', {
             templateUrl: './views/buttons.html',
             controller: 'HomeController',
+            requiresAuth: true
+        })
+        .when('/icons', {
+            templateUrl: './views/icons.html',
+            controller: 'iconsController',
             requiresAuth: true
         })
         .when('/cards', {
@@ -145,6 +150,22 @@ app.controller("HomeController", function ($scope, $rootScope) {
     $rootScope.title = `${title} - Home`;
 });
 
+app.controller("iconsController", function ($scope, $rootScope, $location, $http) {
+    // Resto del código para HomeController
+    $rootScope.title = `${title} - Icons`;
+
+    // Ruta al archivo categories.yml
+    var categoriesUrl = './vendor/fontawesome-free/metadata/categories.yml';
+
+    // Hacer la solicitud HTTP para obtener el contenido del archivo
+    $http.get(categoriesUrl).then(function (response) {
+        // Almacenar los datos en $scope.categories
+        console.log(response);
+        $scope.categories = jsyaml.load(response.data);
+    });
+
+});
+
 app.controller("LoginController", function ($scope, $rootScope, $location, $http) {
     $rootScope.title = `${title} - Login`;
     if (token) {
@@ -154,39 +175,137 @@ app.controller("LoginController", function ($scope, $rootScope, $location, $http
     $scope.submitLogin = function () {
         // Aquí puedes realizar la llamada al API con $http
         var credentials = {
-            username: $scope.username,
+            email: $scope.email,
             password: $scope.password
         };
-        localStorage.setItem('token', "Token de Prueba");
-        setCookie("token", "Token de Prueba", 1);
-        localStorage.setItem('userRole', "admin");
-        $location.path('/home');
-        // $http.post('localhost/api/login', credentials, {
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     }
-        // })
-        //     .then(function (response) {
-        //         console.log(response);
-        //         // Verificar la respuesta del servidor
-        //         if (response.data.success) {
-        //             // Guardar el token en localStorage
-        //             localStorage.setItem('token', response.data.answer.token);
-        //             setCookie("token", response.data.answer.token, 1);
-        //             localStorage.setItem('userRole', response.data.answer.rol);
+        $http({
+            method: 'POST',
+            url: 'api/login',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            // data: Object.keys(credentials)
+            data: serialize(credentials)
+        })
+            .then(function (response) {
+                console.log(response);
+                // Verificar la respuesta del servidor
+                if (response.data.success) {
+                    // Guardar el token en localStorage
+                    // localStorage.setItem('token', response.data.answer.token);
+                    console.log("Supuesto Token", response.data.answer.token);
+                    setCookie("token", response.data.answer.token, 45);
+                    localStorage.setItem('userRole', response.data.answer.rol_alias);
 
-        //             // Redirigir a la página de inicio
-        //             $location.path('/home');
-        //         } else {
-        //             // Manejar el caso de inicio de sesión fallido
-        //             console.log('Inicio de sesión fallido');
-        //         }
-        //     })
-        //     .catch(function (error) {
-        //         console.error('Error al iniciar sesión:', error);
-        //     });
+                    // Redirigir a la página de inicio
+                    $location.path('/home');
+                } else {
+                    // Manejar el caso de inicio de sesión fallido
+                    console.log('Inicio de sesión fallido');
+                }
+            })
+            .catch(function (response) {
+                let errorMessagePrefix = response.data.error.split(':')[0];
+                errorHtml = `
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                         <strong>${errorMessagePrefix}</strong>:${response.data.error.substring(errorMessagePrefix.length + 1)}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                `;
+                $('.showerror').html(errorHtml);
+                console.error('Error al iniciar sesión:', response);
+            });
     };
 
+    startAnimation();
+});
+
+app.controller("RegisterController", function ($scope, $rootScope, $location, $http) {
+    $rootScope.title = `${title} - Register`;
+    if (token) {
+        $location.path('/home');
+    }
+
+    $scope.submitRegister = function () {
+        // Aquí puedes realizar la llamada al API con $http
+        var credentials = {
+            username: $scope.email,
+            email: $scope.email,
+            password: $scope.password
+        };
+        $http({
+            method: 'POST',
+            url: 'api/register',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            // data: Object.keys(credentials)
+            data: serialize(credentials)
+        })
+            .then(function (response) {
+                console.log(response);
+                // Verificar la respuesta del servidor
+                if (response.data.success) {
+                    // Guardar el token en localStorage
+                    // localStorage.setItem('token', response.data.answer.token);
+                    console.log("Supuesto Token", response.data.answer.token);
+                    setCookie("token", response.data.answer.token, 45);
+                    localStorage.setItem('userRole', response.data.answer.rol_alias);
+
+                    // Redirigir a la página de inicio
+                    $location.path('/home');
+                } else {
+                    // Manejar el caso de inicio de sesión fallido
+                    console.log('Inicio de sesión fallido');
+                }
+            })
+            .catch(function (response) {
+                let errorMessagePrefix = response.data.error.split(':')[0];
+                errorHtml = `
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                         <strong>${errorMessagePrefix}</strong>:${response.data.error.substring(errorMessagePrefix.length + 1)}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                `;
+                $('.showerror').html(errorHtml);
+                console.error('Error al iniciar sesión:', response);
+            });
+    };
+
+    $scope.checkPasswordMatch = function () {
+        return ($scope.password) ? $scope.password === $scope.rePassword : false;
+    };
+
+    $scope.checkPasswordPattern = function () {
+        // Verificar si la contraseña cumple con el patrón
+        var passwordPattern = /^(?=.*[!@#$%^&*])(?=.*[a-zA-Z0-9]).{8,}$/;
+
+        // Mensaje de error para la contraseña
+        $scope.passwordErrorMessage = "8 characters, at least 1 special character.";
+
+        // Devolver true si la contraseña cumple con el patrón
+        return passwordPattern.test($scope.password);
+    };
+
+    $scope.transformUsername = function () {
+        if ($scope.username) {
+            // Reemplazar espacios por -
+            $scope.username = $scope.username.replace(/\s+/g, '-');
+
+            // Convertir a minúsculas
+            $scope.username = $scope.username.toLowerCase();
+        }
+    };
+    // $(document).on("keyup", "[ng-model='username']", function (event) {
+    //     // Asegúrate de que el código de AngularJS se ejecute dentro del ciclo de digest
+    //     // $timeout(function () {
+    //         $scope.transformUsername();
+    //     // });
+    // });
     startAnimation();
 });
 
@@ -210,14 +329,15 @@ function val_login($location, $rootScope, nextRoute) {
     var requiresAuth = nextRoute.$$route && nextRoute.$$route.requiresAuth;
 
     // Verificar si el token está presente en localStorage o cookies
-    token = localStorage.getItem('token') || getCookie('token');
+    token = getCookie('token');
     // token = "Token De Prueba";
+    console.log("token", token);
 
     $rootScope.hasSession = token ? true : false;
 
     if (requiresAuth && !token) {
         // Redirigir a la página de inicio de sesión si no hay token
-        $location.path('/login');
+        $location.path('/logout');
     }
 }
 
@@ -226,18 +346,50 @@ function checkUserRole(requiredRoles) {
     return requiredRoles.includes(userRole);
 }
 
-// Función para obtener el valor de una cookie por nombre
+// Función para Obtener el valor una cookie
+// function getCookie(name) {
+//     var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+//     return match ? match[2] : null;
+// }
 function getCookie(name) {
     var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    return match ? match[2] : null;
+
+    if (match) {
+        // Obtener el valor de la cookie
+        var cookieValue = match[2];
+
+        // Comprobar si la cookie tiene fecha de caducidad
+        if (match[1].indexOf(';') === 0) {
+            // Obtener la fecha de caducidad de la cookie
+            var expirationDate = new Date(match[1].slice(1));
+
+            // Comprobar si la cookie ha caducado
+            var currentDate = new Date();
+
+            if (expirationDate > currentDate) {
+                // La cookie no ha caducado, devolver el valor
+                return cookieValue;
+            } else {
+                // La cookie ha caducado, eliminarla
+                document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+                return null;
+            }
+        } else {
+            // La cookie no tiene fecha de caducidad, devolver el valor
+            return cookieValue;
+        }
+    } else {
+        // No se encontró la cookie
+        return null;
+    }
 }
 
 // Función para Crear una cookie
-function setCookie(name, value, days) {
+function setCookie(name, value, minutes) {
     var expires = "";
-    if (days) {
+    if (minutes) {
         var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + value + expires + "; path=/";
@@ -253,4 +405,10 @@ function startAnimation() {
         direction: 'alternate',
         loop: true
     });
+}
+
+function serialize(datos) {
+    return Object.keys(datos).map(function (key) {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(datos[key]);
+    }).join('&')
 }
